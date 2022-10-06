803875585042b3b977c746f3cfceccb1d6916fc7
leonardar <leonardar88@gmail.com>
2022-10-06 13:36:14 +0300

    Update: get_products

--- api/v1/product/get_products.py
+++ api/v1/product/get_products.py
@@ -18,65 +18,109 @@ from src.sql.on_sql_func import dict_transform
 async def async_get_product(filters, user_id):
     async with async_session() as session:
         async with session.begin():
-            query_id = True
-            query_article = True
-            query_country = True
-            query_code = True
-            if filters:
-                if 'id' in filters:
-                    query_id = Product.id == filters['id']
-                if 'article' in filters:
-                    query_article = Product.article == filters['article']
-                if 'country' in filters:
-                    query_country = Product.country == filters['country']
-                if 'code' in filters:
-                    query_code = Product.code == filters['code']
-            result = await session.execute(
-                select(Product, Offer).where(query_id).where(query_article).where(query_country).where(
-                    query_code))
+            users_only = False
+            if "user_offers_only" in filters and filters['user_offers_only'] == 'true':
+                users_only = True
+
             out = []
-            for a in result.scalars().unique():
-                if 'user_offers_only' in filters:
-                    indicator = True
-                else:
-                    indicator = False
-                for offer in a.offers:
-                    if offer.user_id == user_id:
-                        out.append({
-                            "id": a.id,
-                            "article": a.article,
-                            "barcode": a.barcode,
-                            "name": a.name,
-                            "country": {"code": a.country.code,
-                                        "emoji": a.country.emoji,
-                                        "id": a.country.id,
-                                        "name": a.country.name},
-                            "offer": {"id": offer.id,
-                                      "price": float(offer.price),
-                                      "product_id": offer.product_id,
-                                      "quantity": offer.quantity,
-                                      "user_id": offer.user_id,
-                                      "updated": str(offer.updated)},
-                            "photo": a.photo,
-                            "code": a.code,
-                            "updated": str(a.updated),
-                        })
-                        indicator = True
-                        break
-                if not indicator:
-                    out.append({
-                        "id": a.id,
-                        "article": a.article,
-                        "barcode": a.barcode,
-                        "name": a.name,
-                        "country": {"code": a.country.code,
-                                    "emoji": a.country.emoji,
-                                    "id": a.country.id,
-                                    "name": a.country.name},
-                        "photo": a.photo,
-                        "code": a.code,
-                        "updated": str(a.updated),
-                    })
+            if users_only:
+                query = (
+                    f"SELECT DISTINCT products.id AS id, products.article AS article, products.barcode AS barcode, "
+                    f"products.name AS name, countries.code AS country_code, countries.emoji AS country_emoji,"
+                    f"countries.id AS country_id,countries.name AS country_name, offers.id AS offer_id, offers.price AS "
+                    f"offer_price, offers.product_id AS offer_product_id,offers.quantity AS offer_quantity,"
+                    f"offers.user_id AS offer_user_id, offers.updated AS offer_updated, products.photo AS photo,"
+                    f"products.code AS code, products.updated AS updated FROM offers JOIN products ON "
+                    f"offers.product_id = products.id JOIN countries ON products.country_id = countries.id"
+                )
+                prefix = " WHERE "
+                if filters:
+                    if "country" in filters:
+                        query += prefix + f"countries.name = '{filters['country']}'"
+                        prefix = " AND "
+                    if "id" in filters:
+                        query += prefix + f"products.id = '{filters['id']}'"
+                        prefix = " AND "
+                    if "article" in filters:
+                        query += prefix + f"products.article = '{filters['article']}'"
+                        prefix = " AND "
+                    if "code" in filters:
+                        query += prefix + f"products.code = '{filters['code']}'"
+                        prefix = " AND "
+                    query += prefix + f"offers.user_id = '{user_id}'"
+                result = await session.execute(query)
+
+                for a in result:
+                    logging.info(f"{a}")
+                    logging.info(f"{a[12]} == {user_id}")
+                    #if a[12] == user_id:
+                    out.append(
+                        {
+                            "id": a[0],
+                            "article": a[1],
+                            "barcode": a[2],
+                            "name": a[3],
+                            "country": {
+                                "code": a[4],
+                                "emoji": a[5],
+                                "id": a[6],
+                                "name": a[7],
+                            },
+                            "offer": {
+                                "id": a[8],
+                                "price": float(a[9]),
+                                "product_id": a[10],
+                                "quantity": a[11],
+                                "user_id": a[12],
+                                "updated": str(a[13]),
+                            },
+                            "photo": a[14],
+                            "code": a[15],
+                            "updated": str(a[16]),
+                        }
+                    )
+            else:
+                query = (
+                    f"SELECT DISTINCT products.id AS id, products.article AS article, products.barcode AS barcode, "
+                    f"products.name AS name, countries.code AS country_code, countries.emoji AS country_emoji,"
+                    f"countries.id AS country_id,countries.name AS country_name, products.photo AS photo,"
+                    f"products.code AS code, products.updated AS updated FROM products "
+                    f"JOIN countries ON products.country_id = countries.id"
+                )
+                prefix = " WHERE "
+                if filters:
+                    if "country" in filters:
+                        query += prefix + f"countries.name = '{filters['country']}'"
+                        prefix = " AND "
+                    if "id" in filters:
+                        query += prefix + f"products.id = '{filters['id']}'"
+                        prefix = " AND "
+                    if "article" in filters:
+                        query += prefix + f"products.article = '{filters['article']}'"
+                        prefix = " AND "
+                    if "code" in filters:
+                        query += prefix + f"products.code = '{filters['code']}'"
+                result = await session.execute(query)
+
+                for a in result:
+                    logging.info(f"{a}")
+                    out.append(
+                        {
+                            "id": a[0],
+                            "article": a[1],
+                            "barcode": a[2],
+                            "name": a[3],
+                            "country": {
+                                "code": a[4],
+                                "emoji": a[5],
+                                "id": a[6],
+                                "name": a[7],
+                            },
+                            "photo": a[8],
+                            "code": a[9],
+                            "updated": str(a[10]),
+                        }
+                    )
     return out
 
 
