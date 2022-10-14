efb673fc925fbff725afa42df507c2b7e6d19cd7
Andrew <Fastpeaple@gmail.com>
2022-10-14 14:23:37 +0300

    v1.0.3 fix another anna's bug

--- src/sql/connection.py
+++ src/sql/connection.py
@@ -8,13 +8,13 @@ DB_USER = "OdnodvortsevAndrey"
 DB_PASS = "sk*o6j0YO5%"
 
 DB_LOCAL_HOST = "localhost"
-DB_LOCAL_PORT = 5434
+DB_LOCAL_PORT = 5432
 DB_LOCAL_NAME = "provider_api"
 DB_LOCAL_USER = "admin"
 DB_LOCAL_PASS = "2001"
 
 DB_TEST_HOST = "localhost"
-DB_TEST_PORT = 5434
+DB_TEST_PORT = 5432
 DB_TEST_NAME = "provider_api"
 DB_TEST_USER = "admin"
 DB_TEST_PASS = "2001"
