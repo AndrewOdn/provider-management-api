abfc3da1cbd92d3a80847965d73ae7fc69eb9153
Andrew <Fastpeaple@gmail.com>
2022-10-21 13:46:19 +0300

    v1.0.5 get_products.py refactoring

--- src/sql/connection.py
+++ src/sql/connection.py
@@ -8,6 +8,6 @@ from sqlalchemy.orm import sessionmaker
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 engine = create_async_engine(
-    f"postgresql+asyncpg://{DB_USER}:" f"{DB_PASS}@{DB_HOST}:" f"{DB_PORT}/{DB_NAME}", poolclass=NullPool
+    f"postgresql+asyncpg://{DB_USER}:" f"{DB_PASS}@{DB_HOST}:" f"{DB_PORT}/{DB_NAME}", poolclass=NullPool,echo=True
 )
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
